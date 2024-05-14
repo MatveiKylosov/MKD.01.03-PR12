04daf8add8603ff57eab4de3e5ed88b77452f020
MatveiKylosov <matveikyl@mail.ru>
2024-05-14 21:10:27 +0500

    Разработанная функция AlertDialog. Кылосов

--- app/src/main/java/com/example/pr_12/MainActivity.java
+++ app/src/main/java/com/example/pr_12/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.pr_12;
 
 import android.os.Bundle;
-
+import android.content.DialogInterface;
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,6 +11,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void AlertDialog(String title, String message){
+        // Создает и отображает диалоговое окно с заданным заголовком и сообщением.
+        // Диалоговое окно не может быть отменено и содержит кнопку “OK”, которая закрывает диалог.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("OK", (dialog, which) -> dialog.cancel());
+
+        AlertDialog alter = builder.create();
+        alter.show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
