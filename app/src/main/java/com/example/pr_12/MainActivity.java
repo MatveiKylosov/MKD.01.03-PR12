c9878c153a4dcfbda66a29d86c8b0e2ccc558209
MatveiKylosov <matveikyl@mail.ru>
2024-05-14 21:46:03 +0500

    Выполненная практическая работа на оценку удовлетворительно. Кылосов

--- app/src/main/java/com/example/pr_12/MainActivity.java
+++ app/src/main/java/com/example/pr_12/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.pr_12;
 
 import android.os.Bundle;
 import android.content.DialogInterface;
+import android.provider.Telephony;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText FullName;
+    EditText Phone;
+    EditText Address;
 
     public void AlertDialog(String title, String message){
         // Создает и отображает диалоговое окно с заданным заголовком и сообщением.
@@ -23,8 +31,36 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alter = builder.create();
         alter.show();
     }
+
+    public void OnArrange(View view){
+        FullName = findViewById(R.id.FullName);
+        Phone = findViewById(R.id.Phone);
+        Address = findViewById(R.id.Address);
+
+        if(FullName.getText().length() == 0)
+        {
+            AlertDialog("Уведомление", "Пожалуйста, укажите ФИО.");
+            return;
+        }
+
+        if(Phone.getText().length() == 0)
+        {
+            AlertDialog("Уведомление", "Пожалуйста, укажите номер телефона.");
+            return;
+        }
+
+        if(Address.getText().length() == 0)
+        {
+            AlertDialog("Уведомление", "Пожалуйста, укажите адрес доставки.");
+            return;
+        }
+
+        AlertDialog("Уведомление", "Заказ оформлен.");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
